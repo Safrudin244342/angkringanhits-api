edf032ab577de28b5372aae88dcc4d6c6b93908d
muhamad safrudin abdul azis <safrudin2443420@gmail.com>
2020-08-28 17:33:25 +0700

    delete test.js

--- test.js
+++
@@ -1,5 +0,0 @@
-const data = 'aizs'
-const dataArray = 'coco pandan'
-console.log(data.indexOf('s'))
-const badChar = ["'", '<', '>']
-if (badChar.some(badChar => dataArray.indexOf(badChar) > 0)) console.log('tidak aman')
