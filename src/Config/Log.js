7c5c4ff72d0e337888c2384be4025e83afeb1a91
muhamad safrudin abdul azis <safrudin2443420@gmail.com>
2020-10-03 04:12:17 +0700

    fixed bug when create log file

--- src/Config/Log.js
+++ src/Config/Log.js
@@ -1,7 +1,18 @@
 const LOG = require('simple-node-logger')
+const fs = require('fs')
+
+const fileLog = `log/${new Date().getDate()}-${new Date().getMonth()}-${new Date().getFullYear()}.log`
+
+try {
+  if (!fs.existsSync(fileLog)){
+    fs.writeFileSync(fileLog)
+  }
+} catch (err) {
+  console.log(err)
+}
 
 const opts = {
-  logFilePath:`log/${new Date().getDate()}-${new Date().getMonth()}-${new Date().getFullYear()}.log`,
+  logFilePath: fileLog,
   timestampFormat:'YYYY-MM-DD HH:mm:ss.SSS'
 }
 
