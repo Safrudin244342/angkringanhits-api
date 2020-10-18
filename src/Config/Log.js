cbe00807f8b2fcd447e9d59aece73568de31202d
muhamad safrudin abdul azis <safrudin2443420@gmail.com>
2020-10-18 16:40:51 +0700

    fix bug when write log

--- src/Config/Log.js
+++ src/Config/Log.js
@@ -4,6 +4,9 @@ const fs = require('fs')
 const fileLog = `log/${new Date().getDate()}-${new Date().getMonth()}-${new Date().getFullYear()}.log`
 
 try {
+  if (!fs.existsSync('log')) {
+    fs.mkdir("./new-directory-name")
+  }
   if (!fs.existsSync(fileLog)){
     fs.writeFileSync(fileLog, "\n")
   }
